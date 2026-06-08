81abc89f3b242c6dd4b725c91aee60278c36f451
bzp99 <bpeter@edu.bme.hu>
2026-06-08 13:11:48 +0200

    feat(api): disallow passing JSON schema as URL

--- dva-processing/src/dva_processing/engines/json_schema.py
+++ dva-processing/src/dva_processing/engines/json_schema.py
@@ -2,21 +2,15 @@ import html
 import json
 from typing import Any
 
-import requests
-import validators
 from jsonschema import ValidationError, validate as jsvalidate
 
 from ..model import JSONSchemaValidationResult
 
 
 def validate(data: Any, schema: str) -> JSONSchemaValidationResult:
-    # If schema looks like a URL, resolve it;
-    # otherwise treat load it as a JSON schema string
-    if validators.url(schema):
-        schema = requests.get(schema).json()
-    else:
-        # TODO: Can we eliminate this unescape?
-        schema = json.loads(html.unescape(schema))
+    # TODO: Do we want to allow loading from arbitrary URL?
+    # TODO: Can we eliminate this unescape?
+    schema = json.loads(html.unescape(schema))
 
     try:
         jsvalidate(instance=data, schema=schema)
